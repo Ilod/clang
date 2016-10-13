7348d6af3fe76aad1b41d7c0a32fbc9cd37df27a
Ilod <rudy.pons@ilod.org>
2016-10-14 00:18:34 +0200

    Add Registry for builtins added by plugins

--- lib/CodeGen/CGBuiltin.cpp
+++ lib/CodeGen/CGBuiltin.cpp
@@ -22,6 +22,7 @@
 #include "clang/Basic/TargetBuiltins.h"
 #include "clang/Basic/TargetInfo.h"
 #include "clang/CodeGen/CGFunctionInfo.h"
+#include "clang/CodeGen/CodeGenPluginEntryPoint.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/IR/CallSite.h"
 #include "llvm/IR/DataLayout.h"
@@ -2592,6 +2593,11 @@ RValue CodeGenFunction::EmitBuiltinExpr(const FunctionDecl *FD,
     return RValue::get(V);
   }
 
+  if (Value *V = EmitPluginBuiltinExpr(BuiltinID, E)) {
+    CharUnits Alignment = getContext().getAlignOfGlobalVarInChars(getContext().CharTy);
+    return RValue::getAggregate(CodeGen::Address(V, Alignment));
+  }
+
   // See if we have a target specific builtin that needs to be lowered.
   if (Value *V = EmitTargetBuiltinExpr(BuiltinID, E))
     return RValue::get(V);
@@ -2635,6 +2641,16 @@ static Value *EmitTargetArchBuiltinExpr(CodeGenFunction *CGF,
   default:
     return nullptr;
   }
+}
+
+Value *CodeGenFunction::EmitPluginBuiltinExpr(unsigned BuiltinID,
+                                              const CallExpr *E) {
+  if (getContext().BuiltinInfo.isPluginBuiltinID(BuiltinID)) {
+    assert(getContext().BuiltinInfo.getPluginBuiltinHandler(BuiltinID) && "Missing builtin handler");
+    return getContext().BuiltinInfo.getPluginBuiltinHandler(BuiltinID)->EmitBuiltin(E, getContext(), CodeGenPluginEntryPoint(CGM));
+  }
+
+  return nullptr;
 }
 
 Value *CodeGenFunction::EmitTargetBuiltinExpr(unsigned BuiltinID,
--- lib/Sema/SemaChecking.cpp
+++ lib/Sema/SemaChecking.cpp
@@ -1067,9 +1067,13 @@ Sema::CheckBuiltinFunctionCall(FunctionDecl *FDecl, unsigned BuiltinID,
       return ExprError();
   }
 
+  if (Context.BuiltinInfo.isPluginBuiltinID(BuiltinID)) {
+    if (Context.BuiltinInfo.getPluginBuiltinHandler(BuiltinID)->CheckBuiltinCall(TheCall, *this))
+      return ExprError();
+  }
   // Since the target specific builtins for each arch overlap, only check those
   // of the arch we are compiling for.
-  if (Context.BuiltinInfo.isTSBuiltin(BuiltinID)) {
+  else if (Context.BuiltinInfo.isTSBuiltin(BuiltinID)) {
     switch (Context.getTargetInfo().getTriple().getArch()) {
       case llvm::Triple::arm:
       case llvm::Triple::armeb:
